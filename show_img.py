74a672ab1e455551d371a0108cb3788beec9bfbf
bijilbaji <bijilbaji@gmail.com>
2022-08-12 00:50:12 +0530

    Update show_img.py

--- show_img.py
+++ show_img.py
@@ -1,4 +1,4 @@
-from IPython.display import image
+from IPython.display import Image
 
-Image("/content/result/result.png")
-Image("/content/result/input.png")
+Image("/content/result/result.jpg")
+Image("/content/result/input.jpg")
