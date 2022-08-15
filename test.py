b447567f8201e6b420e92461f2ace791e83bdb72
bijilbaji <bijilbaji@gmail.com>
2022-08-16 00:47:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from IPython.display import Image
 from inpaint_model import InpaintCAModel
 import neuralgym as ng
 import tensorflow as tf
+tf.get_logger().setLevel('INFO')
 import os
 import argparse
 import cv2
